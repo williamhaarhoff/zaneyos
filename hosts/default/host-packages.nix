276dc51f87dbdb993624c291fa00896377335e84
Will <williamhaarhoff@gmail.com>
2025-06-30 04:46:40 +1200

    Remove vscode as system package

--- hosts/default/host-packages.nix
+++ hosts/default/host-packages.nix
@@ -2,26 +2,6 @@
   environment.systemPackages = with pkgs; [
     discord
     obs-studio
-    (vscode-with-extensions.override {
-      vscodeExtensions = with vscode-extensions; [
-        bbenoist.nix
-        llvm-vs-code-extensions.vscode-clangd
-        ms-vscode.cpptools
-        mkhl.direnv
-        twxs.cmake
-        ms-python.python
-        ms-python.vscode-pylance
-        ms-vscode.cpptools-extension-pack
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      #  {
-      #    name = "s-nlf-fh.glassit";
-      #    publisher = "hikarin522";
-      #    version = "0.2.6";
-      #    sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-      #  }
-     ];
-
-    })
     gitkraken
     just
     jq
