e0d5662085990b6b2299afc9189eb49e3f9e2f23
Will <williamhaarhoff@gmail.com>
2025-07-06 05:34:31 +1200

    Just command runner vscode

--- modules/home/code.nix
+++ modules/home/code.nix
@@ -1,19 +1,29 @@
 {pkgs, ...}: {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      dracula-theme.theme-dracula
-      vscodevim.vim
-      yzhang.markdown-all-in-one
-      bbenoist.nix
-      llvm-vs-code-extensions.vscode-clangd
-      mads-hartmann.bash-ide-vscode
-      #ms-vscode.cpptools
-      #mkhl.direnv
-      twxs.cmake
-      ms-python.python
-      ms-python.vscode-pylance
-    ];
+    extensions = with pkgs.vscode-extensions;
+      [
+        dracula-theme.theme-dracula
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+        bbenoist.nix
+        llvm-vs-code-extensions.vscode-clangd
+        mads-hartmann.bash-ide-vscode
+        #ms-vscode.cpptools
+        #mkhl.direnv
+        twxs.cmake
+        ms-python.python
+        ms-python.vscode-pylance
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "vscode-just-syntax";
+          publisher = "nefrob";
+          version = "0.8.0";
+          sha246 = "sha256-zuDfIxhiUKRpVRxp9BceW6WPBq5NNCuS1Si0/6kfqF8=";
+        }
+      ];
+
     profiles.default.userSettings = {
       terminal = {
         integrated.defaultProfile.linux = "zsh";
