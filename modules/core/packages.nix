80d746e333080c60e01645f0668189e4c73e1ce6
Will <williamhaarhoff@gmail.com>
2025-10-24 04:44:24 +1300

    Added kicad

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -44,6 +44,7 @@
     hyprpicker # Color Picker
     eog # For Image Viewing
     inxi # CLI System Information Tool
+    kicad
     killall # For Killing All Instances Of Programs
     libnotify # For Notifications
     lm_sensors # Used For Getting Hardware Temps
