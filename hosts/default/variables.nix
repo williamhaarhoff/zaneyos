722f5fd2c94153340151a9844c2f91c915345986
Will <williamhaarhoff@gmail.com>
2025-10-24 04:48:29 +1300

    Tower night theme change

--- hosts/default/variables.nix
+++ hosts/default/variables.nix
@@ -31,15 +31,15 @@
   printEnable = false;
 
   # Set Stylix Image
-  stylixImage = ../../wallpapers/Anime-Room.png;
+  stylixImage = ../../wallpapers/Tower-Night.png;
 
   # Set Waybar
   #  Available Options:
   #waybarChoice = ../../modules/home/waybar/waybar-simple.nix;
   #waybarChoice = ../../modules/home/waybar/waybar-ddubs.nix;
   #waybarChoice = ../../modules/home/waybar/waybar-ddubs-2.nix;
-  #waybarChoice = ../../modules/home/waybar/Jerry-waybar.nix;
-  waybarChoice = ../../modules/home/waybar/waybar-curved.nix;
+  waybarChoice = ../../modules/home/waybar/Jerry-waybar.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-curved.nix;
 
   # Set Animation style
   # Available options are:
